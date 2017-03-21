4db98d05b71537678e4c93698c3678f4d6307c11
Andy McCall <mailme@andymccall.co.uk>
2017-03-21 22:01:15 +0000

    First release

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# pchr.andymccall.co.uk
-GUI for showing the md5sum of strategy files for Experian Powercurve Originations
+#Powercurve Hash Reporter
+
+GUI for showing the md5sum of strategy files for Experian Powercurve Originations.
+
+##Build Status
+
+##Usage
+
+To configure strategy locations edit applications.properties to point to the relevant strategy files.
+
+Supported configuration options are:
+
+* file.location.pre=\<location of PRE.jar\>
+* file.location.post=\<location of POST.jar\>
+* file.location.bc=\<location of Business Process Composition.bpc.zip\>
+* file.location.dbpdm=\<location of Business Process Composition.dbpdm.zip\>
+
+##License
+
+Powercurve Hash Reporter is licensed under GPLv3. A copy of which can be found in this repository.
--- src/main/java/uk/co/andymccall/pchr/Main.java
+++ src/main/java/uk/co/andymccall/pchr/Main.java
@@ -2,22 +2,21 @@ package uk.co.andymccall.pchr;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import javax.xml.bind.DatatypeConverter;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
@@ -31,59 +30,72 @@ public class Main extends Application {
     private static String preFileLocation;
     private static String postFileLocation;
     private static String bcFileLocation;
-    private static String bcdmFileLocation;
+    private static String dbpdmFileLocation;
+
+    Label preLabel = new Label("PRE.jar");
+    Label postLabel = new Label("POST.jar");
+    Label bcLabel = new Label("Business Process Composition.bpc.zip");
+    Label dbpdmLabel = new Label("Business Process Composition.dbpdm.zip");
+
+    TextField preHash = new TextField();
+    TextField postHash = new TextField();
+    TextField bcHash = new TextField();
+    TextField dbpdmHash = new TextField();
 
     @Override
     public void start(Stage primaryStage) {
+
+        final FileChooser fileChooser = new FileChooser();
+
+
         primaryStage.setTitle("Powercurve Hash Reporter");
 
         BorderPane root = new BorderPane();
         VBox topContainer = new VBox();
+
 
         MenuBar mainMenu = new MenuBar();
         mainMenu.prefWidthProperty().bind(primaryStage.widthProperty());
         topContainer.getChildren().add(mainMenu);
 
         Menu fileMenu = new Menu("File");
-        MenuItem newMenuItem = new MenuItem("Refresh");
-        MenuItem saveMenuItem = new MenuItem("Save to File...");
+        MenuItem refreshMenuItem = new MenuItem("Refresh");
+        refreshMenuItem.setOnAction(actionEvent -> updateAllHashes());
+
+        MenuItem exportMenuItem = new MenuItem("Export to File...");
+        exportMenuItem.setOnAction(actionEvent -> exportAllHashes(primaryStage));
+
         MenuItem exitMenuItem = new MenuItem("Exit");
         exitMenuItem.setOnAction(actionEvent -> Platform.exit());
 
-        fileMenu.getItems().addAll(newMenuItem, saveMenuItem,
+        fileMenu.getItems().addAll(refreshMenuItem, exportMenuItem,
                 new SeparatorMenuItem(), exitMenuItem);
-
-        Menu editMenu = new Menu("Edit");
-        MenuItem copyMenuItem = new MenuItem("Copy");
-        editMenu.getItems().add(copyMenuItem);
 
         Menu helpMenu = new Menu("Help");
         MenuItem aboutMenuItem = new MenuItem("About");
         helpMenu.getItems().add(aboutMenuItem);
-        aboutMenuItem.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent t) {
-                aboutDialog();
-            }
-        });
+        aboutMenuItem.setOnAction(actionEvent -> aboutDialog());
 
-
-        mainMenu.getMenus().addAll(fileMenu, editMenu, helpMenu);
+        mainMenu.getMenus().addAll(fileMenu, helpMenu);
 
         ToolBar toolBar = new ToolBar();
 
         Button refreshButton = new Button();
         refreshButton.setGraphic(new ImageView("/icons/refresh.png"));
         refreshButton.setTooltip(new Tooltip("Refresh the hashes"));
+        refreshButton.setOnAction(actionEvent -> updateAllHashes());
         toolBar.getItems().add(refreshButton);
 
         Button copyButton = new Button();
         copyButton.setGraphic(new ImageView("/icons/copy.png"));
         copyButton.setTooltip(new Tooltip("Copy the hashes to the clipboard"));
+        copyButton.setOnAction(actionEvent -> copyToClipBoard(getAllHashes()));
         toolBar.getItems().add(copyButton);
 
         Button exportButton = new Button();
         exportButton.setGraphic(new ImageView("/icons/export.png"));
         exportButton.setTooltip(new Tooltip("Export the hashes to a file..."));
+        exportButton.setOnAction(actionEvent -> exportAllHashes(primaryStage));
         toolBar.getItems().add(exportButton);
 
         toolBar.prefWidthProperty().bind(primaryStage.widthProperty());
@@ -95,111 +107,77 @@ public class Main extends Application {
         hashes.setPadding(new Insets(10, 10, 10, 10));
         hashes.setSpacing(10);
 
-        String hash;
 
-        try {
-            hash = getFileHash(preFileLocation);
+        preHash.setPrefColumnCount(24);
+        preHash.setEditable(false);
+        updateHash(preHash, preFileLocation);
 
-            Label preLabel = new Label("PRE.jar");
+        Button preCopyButton = new Button();
+        preCopyButton.setText("Copy");
+        preCopyButton.setTooltip(new Tooltip("Copy the PRE.jar hash"));
+        preCopyButton.setOnAction(actionEvent -> copyToClipBoard(preLabel.getText() + " : " + preHash.getText()));
 
-            TextField preHash = new TextField();
-            preHash.setPrefColumnCount(24);
-            preHash.setText(hash);
-            preHash.setEditable(false);
+        HBox preBox = new HBox();
+        preBox.setAlignment(BASELINE_RIGHT);
+        preBox.getChildren().addAll(preLabel, preHash, preCopyButton);
+        preBox.setSpacing(10);
 
-            Button preCopyButton = new Button();
-            preCopyButton.setText("Copy");
+        hashes.getChildren().add(preBox);
 
-            HBox preBox = new HBox();
-            preBox.setAlignment(BASELINE_RIGHT);
-            preBox.getChildren().addAll(preLabel, preHash,preCopyButton);
-            preBox.setSpacing(10);
+        postHash.setPrefColumnCount(24);
+        postHash.setEditable(false);
+        updateHash(postHash, postFileLocation);
 
-            hashes.getChildren().add(preBox);
-
-
-        } catch (IOException | NoSuchAlgorithmException e) {
-            System.out.println(e.toString());
-        }
-
-        try {
-            hash = getFileHash(postFileLocation);
-
-            Label postLabel = new Label("POST.jar");
-
-            TextField postHash = new TextField();
-            postHash.setPrefColumnCount(24);
-            postHash.setText(hash);
-            postHash.setEditable(false);
-
-            Button postCopyButton = new Button();
-            postCopyButton.setText("Copy");
-
-            HBox postBox = new HBox();
-            postBox.setAlignment(BASELINE_RIGHT);
-            postBox.getChildren().addAll(postLabel, postHash,postCopyButton);
-            postBox.setSpacing(10);
-
-            hashes.getChildren().add(postBox);
+        Button postCopyButton = new Button();
+        postCopyButton.setText("Copy");
+        postCopyButton.setTooltip(new Tooltip("Copy the POST.jar hash"));
+        postCopyButton.setOnAction(actionEvent -> copyToClipBoard(postLabel.getText() + " : " + postHash.getText()));
 
 
-        } catch (IOException | NoSuchAlgorithmException e) {
-            System.out.println(e.toString());
-        }
+        HBox postBox = new HBox();
+        postBox.setAlignment(BASELINE_RIGHT);
+        postBox.getChildren().addAll(postLabel, postHash, postCopyButton);
+        postBox.setSpacing(10);
 
-        try {
-            hash = getFileHash(bcFileLocation);
-
-            Label bcLabel = new Label("Business Composition.jar");
-
-            TextField bcHash = new TextField();
-            bcHash.setPrefColumnCount(24);
-            bcHash.setText(hash);
-            bcHash.setEditable(false);
-
-            Button bcCopyButton = new Button();
-            bcCopyButton.setText("Copy");
-
-            HBox postBox = new HBox();
-            postBox.setAlignment(BASELINE_RIGHT);
-            postBox.getChildren().addAll(bcLabel, bcHash,bcCopyButton);
-            postBox.setSpacing(10);
-
-            hashes.getChildren().add(postBox);
+        hashes.getChildren().add(postBox);
 
 
-        } catch (IOException | NoSuchAlgorithmException e) {
-            System.out.println(e.toString());
-        }
+        bcHash.setPrefColumnCount(24);
+        bcHash.setEditable(false);
+        updateHash(bcHash, bcFileLocation);
+
+        Button bcCopyButton = new Button();
+        bcCopyButton.setText("Copy");
+        bcCopyButton.setTooltip(new Tooltip("Copy the Business Process Composition.bpc.zip hash"));
+        bcCopyButton.setOnAction(actionEvent -> copyToClipBoard(bcLabel.getText() + " : " + bcHash.getText()));
 
 
-        try {
-            hash = getFileHash(bcdmFileLocation);
+        HBox bcBox = new HBox();
+        bcBox.setAlignment(BASELINE_RIGHT);
+        bcBox.getChildren().addAll(bcLabel, bcHash, bcCopyButton);
+        bcBox.setSpacing(10);
 
-            Label dbdmLabel = new Label("Business Composition Database.jar");
+        hashes.getChildren().add(bcBox);
 
-            TextField dbdmHash = new TextField();
-            dbdmHash.setPrefColumnCount(24);
-            dbdmHash.setText(hash);
-            dbdmHash.setEditable(false);
+        dbpdmHash.setPrefColumnCount(24);
+        dbpdmHash.setEditable(false);
+        updateHash(dbpdmHash, dbpdmFileLocation);
 
-            Button dbdmCopyButton = new Button();
-            dbdmCopyButton.setText("Copy");
+        Button dbpdmCopyButton = new Button();
+        dbpdmCopyButton.setText("Copy");
+        dbpdmCopyButton.setTooltip(new Tooltip("Copy the Business Process Composition.dbpdm.zip hash"));
+        dbpdmCopyButton.setOnAction(actionEvent -> copyToClipBoard(dbpdmLabel.getText() + " : " + dbpdmHash.getText()));
 
-            HBox postBox = new HBox();
-            postBox.setAlignment(BASELINE_RIGHT);
-            postBox.getChildren().addAll(dbdmLabel, dbdmHash,dbdmCopyButton);
-            postBox.setSpacing(10);
+        HBox dbpdmBox = new HBox();
+        dbpdmBox.setAlignment(BASELINE_RIGHT);
+        dbpdmBox.getChildren().addAll(dbpdmLabel, dbpdmHash, dbpdmCopyButton);
+        dbpdmBox.setSpacing(10);
 
-            hashes.getChildren().add(postBox);
-
-        } catch (IOException | NoSuchAlgorithmException e) {
-            System.out.println(e.toString());
-        }
+        hashes.getChildren().add(dbpdmBox);
 
         topContainer.getChildren().add(hashes);
 
-        Scene scene = new Scene(root, 625, 250, Color.WHITE);
+        Scene scene = new Scene(root, 665, 250, Color.WHITE);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
@@ -227,6 +205,63 @@ public class Main extends Application {
                 .printHexBinary(digest).toUpperCase();
     }
 
+    public void updateHash(TextField textField, String fileLocation) {
+        String hash;
+        try {
+            hash = getFileHash(fileLocation);
+            textField.setText(hash);
+        } catch (IOException e) {
+            textField.setText("Unable to read file!");
+        } catch (NoSuchAlgorithmException e) {
+            textField.setText("Unable to generate hash!");
+        }
+    }
+
+    public void updateAllHashes() {
+        updateHash(preHash, preFileLocation);
+        updateHash(postHash, postFileLocation);
+        updateHash(bcHash, bcFileLocation);
+        updateHash(dbpdmHash, dbpdmFileLocation);
+    }
+
+    public void copyToClipBoard(String string) {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent content = new ClipboardContent();
+        content.putString(string);
+        clipboard.setContent(content);
+    }
+
+    public String getAllHashes() {
+        String copy;
+
+        copy = preLabel.getText() + " : " + preHash.getText() + "\r\n" +
+                postLabel.getText() + " : " + postHash.getText() + "\r\n" +
+                bcLabel.getText() + " : " + bcHash.getText() + "\r\n" +
+                dbpdmLabel.getText() + " : " + dbpdmHash.getText();
+
+        return copy;
+    }
+
+    public void exportAllHashes(Stage primaryStage) {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showSaveDialog(primaryStage);
+        if (file != null && !file.exists() && file.getParentFile().isDirectory()) {
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(file);
+                writer.write(getAllHashes());
+                writer.flush();
+            } catch (IOException ex) {
+                System.out.println(ex.toString());
+            } finally {
+                try {
+                    writer.close();
+                } catch (IOException ex) {
+                    System.out.println(ex.toString());
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -237,13 +272,12 @@ public class Main extends Application {
 
             input = new FileInputStream("application.properties");
 
-            // load a properties file
             prop.load(input);
 
-            preFileLocation=prop.getProperty("file.location.pre");
-            postFileLocation=prop.getProperty("file.location.post");
-            bcFileLocation=prop.getProperty("file.location.bc");
-            bcdmFileLocation=prop.getProperty("file.location.bcdm");
+            preFileLocation = prop.getProperty("file.location.pre");
+            postFileLocation = prop.getProperty("file.location.post");
+            bcFileLocation = prop.getProperty("file.location.bc");
+            dbpdmFileLocation = prop.getProperty("file.location.dbpdm");
 
 
         } catch (IOException ex) {
